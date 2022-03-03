fd078aa87a93516524de329c0587e2ab10d965e3
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-03-03 16:26:31 -0300

    feat/styled Input

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,8 +1,10 @@
 export default {
   colors: {
-    black: '#000e1a',
+    black: '#000',
     white: '#fff',
     blue: '#007ce0',
-    dark: '#212121'
+    dark: '#212121',
+    borderDark: '#424242',
+    gray: '#9E9E9E'
   }
 }
