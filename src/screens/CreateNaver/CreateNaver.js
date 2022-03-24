4fe99fcb1c275c3a23d6938b4df1d4c40f1c3d6a
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-03-24 17:11:31 -0300

    fix/ remove unnecessary imports, Row's and View's

--- src/screens/CreateNaver/CreateNaver.js
+++ src/screens/CreateNaver/CreateNaver.js
@@ -1,61 +1,55 @@
 import React, { useState } from 'react'
 import { View, ScrollView } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
 
-import { Button, Column, Header, Input, Row, Text } from '../../components'
+import { Button, Column, Header, Input, Text } from 'src/components'
 
-const CreateNaver = ({ route }) => {
+const CreateNaver = ({ route, navigation }) => {
   const [editNaver, setEditNaver] = useState(route.params?.naver)
-  const navigation = useNavigation()
 
   return (
     <View alignItems='center' justifyContent='center'>
-      <Row>
-        <Header backHeader />
-      </Row>
+      <Header backHeader />
       <Text fontFamily='MontserratSemiBold' color='black' fontSize='22px' my={24}>
         {`${!!route.params?.naver?.name ? 'Editar' : 'Criar'} Naver`}
       </Text>
       <ScrollView width='100%'>
         <Column px={16}>
-          <View>
-            <Input
-              label='Nome'
-              placeholder='Nome'
-              value={editNaver?.name}
-              onChange={text => setEditNaver({ ...editNaver, name: text })}
-            />
-            <Input
-              label='Cargo'
-              placeholder='Cargo'
-              value={editNaver?.job_role}
-              onChange={text => setEditNaver({ ...editNaver, name: text })}
-            />
-            <Input
-              label='Idade'
-              placeholder='Idade'
-              value={editNaver?.birthdate}
-              onChange={text => setEditNaver({ ...editNaver, name: text })}
-            />
-            <Input
-              label='Tempo de empresa'
-              placeholder='Tempo de empresa'
-              value={editNaver?.admission_date}
-              onChange={text => setEditNaver({ ...editNaver, name: text })}
-            />
-            <Input
-              label='Projetos que participou'
-              placeholder='Projetos que participou'
-              value={editNaver?.project}
-              onChange={text => setEditNaver({ ...editNaver, name: text })}
-            />
-            <Input
-              label='URL da foto do naver'
-              placeholder='URL da foto do naver'
-              value={editNaver?.url}
-              onChange={text => setEditNaver({ ...editNaver, name: text })}
-            />
-          </View>
+          <Input
+            label='Nome'
+            placeholder='Nome'
+            value={editNaver?.name}
+            onChange={text => setEditNaver({ ...editNaver, name: text })}
+          />
+          <Input
+            label='Cargo'
+            placeholder='Cargo'
+            value={editNaver?.job_role}
+            onChange={text => setEditNaver({ ...editNaver, job_role: text })}
+          />
+          <Input
+            label='Idade'
+            placeholder='Idade'
+            value={editNaver?.birthdate}
+            onChange={text => setEditNaver({ ...editNaver, birthdate: text })}
+          />
+          <Input
+            label='Tempo de empresa'
+            placeholder='Tempo de empresa'
+            value={editNaver?.admission_date}
+            onChange={text => setEditNaver({ ...editNaver, admission_date: text })}
+          />
+          <Input
+            label='Projetos que participou'
+            placeholder='Projetos que participou'
+            value={editNaver?.project}
+            onChange={text => setEditNaver({ ...editNaver, project: text })}
+          />
+          <Input
+            label='URL da foto do naver'
+            placeholder='URL da foto do naver'
+            value={editNaver?.url}
+            onChange={text => setEditNaver({ ...editNaver, url: text })}
+          />
 
           <Button
             text='Salvar'
