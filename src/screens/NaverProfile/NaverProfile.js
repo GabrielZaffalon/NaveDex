33af6d1929941227e8ba48f3f5e3da4216e1048e
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-03-25 11:56:40 -0300

    fix/remove unnused useStates and View for Clumn

--- src/screens/NaverProfile/NaverProfile.js
+++ src/screens/NaverProfile/NaverProfile.js
@@ -8,7 +8,7 @@ const NaverProfile = ({ route }) => {
   const [isConfirmingDeletion, setIsConfirmingDeletion] = useState(false)
   const [hasSuccessfullyDeleted, setHasSuccessfullyDeleted] = useState(false)
 
-  const [Naver, setNaver] = useState(route.params?.naver)
+  const { naver } = route.params
 
   const navigation = useNavigation()
 
@@ -16,37 +16,37 @@ const NaverProfile = ({ route }) => {
   const height = width * 0.8
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Column style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Header backHeader />
-      <Image source={{ uri: Naver.url }} style={{ width, height }} />
       <ScrollView>
+        <Image source={{ uri: naver.url }} style={{ width, height }} />
         <Column mx={16}>
           <Text fontFamily='MontserratSemiBold' fontSize={22} color='black' marginTop={24}>
-            {Naver.name}
+            {naver.name}
           </Text>
           <Text fontFamily='MontserratLight' fontSize={16} color='black' marginTop='4px'>
-            {Naver.job_role}
+            {naver.job_role}
           </Text>
 
           <Text fontFamily='MontserratSemiBold' fontSize={16} color='black' marginTop={24}>
             Idade
           </Text>
           <Text fontFamily='MontserratLight' fontSize={16} color='black' marginTop='4px'>
-            {Naver.birthdate}
+            {naver.birthdate}
           </Text>
 
           <Text fontFamily='MontserratSemiBold' fontSize={16} color='black' marginTop={24}>
             Tempo de empresa
           </Text>
           <Text fontFamily='MontserratLight' fontSize={16} color='black' marginTop='4px'>
-            {Naver.admission_date}
+            {naver.admission_date}
           </Text>
 
           <Text fontFamily='MontserratSemiBold' fontSize={16} color='black' marginTop={24}>
             Projetos que participou
           </Text>
           <Text fontFamily='MontserratLight' fontSize={16} color='black' marginTop='4px'>
-            {Naver.project}
+            {naver.project}
           </Text>
         </Column>
 
@@ -62,7 +62,7 @@ const NaverProfile = ({ route }) => {
           />
           <Button
             text='Editar'
-            onPress={() => navigation.navigate('CreateNaver', { Naver })}
+            onPress={() => navigation.navigate('CreateNaver', { naver })}
             variant='primary'
             width='158px'
             height={40}
@@ -94,7 +94,7 @@ const NaverProfile = ({ route }) => {
         title='Naver excluído'
         description='Naver excluído com sucesso'
       />
-    </View>
+    </Column>
   )
 }
 
