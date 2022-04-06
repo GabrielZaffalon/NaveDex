e0043c39b9ff35b9213b7ab1f38a74d68582bc05
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-04-06 14:56:04 -0300

    adds isFocused to home page

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,54 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { View, ScrollView, Dimensions } from 'react-native'
+import { useIsFocused } from '@react-navigation/native'
 
 import { Text, Naver, Row, Modal, Header, Button } from 'src/components'
 import { showNavers } from 'src/services'
 
-const NAVERS = [
-  {
-    id: '1',
-    name: 'Pequeno golden',
-    admission_date: '2020-04-22T00:00:00.000Z',
-    job_role: 'Um golden filhote',
-    user_id: '1',
-    project: 'Nasceu de um golden',
-    birthdate: '2 meses',
-    url: 'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2F2EfHtzauHcM%2Fmaxresdefault.jpg'
-  },
-  {
-    id: '2',
-    name: 'Grande golden',
-    admission_date: '2020-04-22T00:00:00.000Z',
-    job_role: 'Um golden adulto',
-    user_id: '2',
-    project: 'Late',
-    birthdate: '2 anos',
-    url: 'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fmundoaparte.com.br%2Fwp-content%2Fuploads%2F2017%2F09%2Fgolden-retriever-2419453_960_720.jpg'
-  },
-  {
-    id: '3',
-    name: 'Faustão',
-    admission_date: '2020-04-22T00:00:00.000Z',
-    job_role: 'É o Faustão',
-    user_id: '3',
-    project: 'Foi pra Band',
-    birthdate: '71 anos',
-    url: 'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fstatic.poder360.com.br%2F2021%2F07%2Ffaustao.png'
-  },
-  {
-    id: '4',
-    name: 'Golden pequeno',
-    admission_date: '2020-04-22T00:00:00.000Z',
-    job_role: 'É o mesmo mas diferente',
-    user_id: '4',
-    project: 'Nasceu de outro golden',
-    birthdate: '2/5 meses',
-    url: 'https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fi.ytimg.com%2Fvi%2F2EfHtzauHcM%2Fmaxresdefault.jpg'
-  }
-]
-
 const Home = ({ navigation }) => {
   const screenWidth = Dimensions.get('screen').width
+
+  const isFocused = useIsFocused()
 
   const [isConfirmingDeletion, setIsConfirmingDeletion] = useState(false)
   const [hasSuccessfullyDeleted, setHasSuccessfullyDeleted] = useState(false)
@@ -66,7 +26,7 @@ const Home = ({ navigation }) => {
 
   useEffect(() => {
     fetchNavers()
-  }, [])
+  }, [isFocused])
 
   return (
     <View style={{ flex: 1 }}>
